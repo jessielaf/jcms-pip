d113d0dcdbecc245d24d7eedb0c0fd5d35f81a85
Jessie Liauw A Fong <jessielaff@live.nl>
2017-10-17 21:52:27 +0200

    login css

--- build/lib/jcms/templatetags/crud_url.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def crud_url(view_type, model):
-    string = 'jcms:' + model.__class__.__name__.lower() + view_type
-    return string
--- build/lib/jcms/templatetags/get_object_attr.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def get_object_attr(use_object, name):
-    return getattr(use_object, name)
