d448f84066e43853d107d802a186c75d02e75938
Jessie Liauw A Fong <jessielaff@live.nl>
2017-11-09 21:37:06 +0100

    other styling

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,41 @@
 jQuery = $ = require('jquery');
 
 $(document).ready(function () {
-    var menuSelectedClass = 'menu-selected';
+    userMenu();
+    mobileMenu();
 
-    $('.menu-item').on('click', function () {
-        if ($(this).hasClass(menuSelectedClass)) {
-            $(this).removeClass(menuSelectedClass);
-            return;
-        }
+    function userMenu() {
 
-        $('.' + menuSelectedClass).removeClass(menuSelectedClass);
-        $(this).addClass(menuSelectedClass)
-    });
+        $('.menu-item').on('click', function () {
+            var menuSelectedClass = 'menu-selected';
+            
+            if ($(this).hasClass(menuSelectedClass)) {
+                $(this).removeClass(menuSelectedClass);
+                return;
+            }
+
+            $('.' + menuSelectedClass).removeClass(menuSelectedClass);
+            $(this).addClass(menuSelectedClass)
+        });
+    }
+
+    function mobileMenu() {
+
+        $('.mobile-menu').on('click', function () {
+            var sideNav = $('.side-nav');
+            var content = $('.content');
+            
+            var menuOpenClass = 'show-menu';
+            var hiddenContent = 'hidden-content';
+
+            if (sideNav.hasClass(menuOpenClass)) {
+                sideNav.removeClass(menuOpenClass);
+                content.removeClass(hiddenContent);
+            } else {
+                sideNav.addClass(menuOpenClass);
+                content.addClass(hiddenContent);
+            }
+        });
+    }
+
 });
