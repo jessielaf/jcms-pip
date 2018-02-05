555ea0d42d394188900adffcc8c95d30750b31b6
Jessie Liauw A Fong <jessielaff@live.nl>
2018-02-05 22:38:30 +0100

    Added the new icons & added a editor

--- jcms/views/loginViews.py
+++ jcms/views/loginViews.py
@@ -10,6 +10,9 @@ class LoginView(TemplateView):
 
     def get(self, request):
         form = LoginForm()
+        if request.user.is_authenticated():
+            return redirect('jcms:optionList')
+
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
--- src/js/app.js
+++ src/js/app.js
@@ -1,40 +1,46 @@
 jQuery = $ = require('jquery');
+require('trumbowyg');
 
 $(document).ready(function () {
     userMenu();
     mobileMenu();
-
-    function userMenu() {
-
-        $('.menu-item').on('click', function () {
-            var menuSelectedClass = 'menu-selected';
-            
-            if ($(this).hasClass(menuSelectedClass)) {
-                $(this).removeClass(menuSelectedClass);
-                return;
-            }
-
-            $('.' + menuSelectedClass).removeClass(menuSelectedClass);
-            $(this).addClass(menuSelectedClass)
-        });
-    }
-
-    function mobileMenu() {
-
-        $('.mobile-menu').on('click', function () {
-            var sideNav = $('.side-nav');
-            var content = $('.content');
-            
-            var menuOpenClass = 'show-menu';
-            var hiddenContent = 'hidden-content';
-
-            if (sideNav.hasClass(menuOpenClass)) {
-                sideNav.removeClass(menuOpenClass);
-                content.removeClass(hiddenContent);
-            } else {
-                sideNav.addClass(menuOpenClass);
-                content.addClass(hiddenContent);
-            }
-        });
-    }
+    setCkeditor();
 });
+
+function setCkeditor() {
+    $('textarea').trumbowyg({
+        svgPath: "https://raw.githubusercontent.com/Alex-D/Trumbowyg/master/dist/ui/icons.svg"
+    });
+}
+
+function userMenu() {
+    $('.menu-item').on('click', function () {
+        var menuSelectedClass = 'menu-selected';
+
+        if ($(this).hasClass(menuSelectedClass)) {
+            $(this).removeClass(menuSelectedClass);
+            return;
+        }
+
+        $('.' + menuSelectedClass).removeClass(menuSelectedClass);
+        $(this).addClass(menuSelectedClass)
+    });
+}
+
+function mobileMenu() {
+    $('.mobile-menu').on('click', function () {
+        var sideNav = $('.side-nav');
+        var content = $('.content');
+
+        var menuOpenClass = 'show-menu';
+        var hiddenContent = 'hidden-content';
+
+        if (sideNav.hasClass(menuOpenClass)) {
+            sideNav.removeClass(menuOpenClass);
+            content.removeClass(hiddenContent);
+        } else {
+            sideNav.addClass(menuOpenClass);
+            content.addClass(hiddenContent);
+        }
+    });
+}
