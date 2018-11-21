f948dcbff6de68124ed48bd216e9326e84494d3f
Jessie Liauw A Fong <jessielaff@live.nl>
2018-11-21 19:05:54 +0100

    change version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.1.6',
+    version='1.2',
 
     description='This is a cms written in Django and made by JCB Development',
     long_description=long_description,
